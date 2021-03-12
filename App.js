8dbbdd385a5aea5ff27bb0c45f2403cb4f2588ff
sooraj <soorajdube.is@gmail.com>
2021-03-12 12:32:40 +0530

    Sooraj|Added functionality to send message to server

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import RemoteMouseService from './services/remote-mouse-service'
+import getWebSocketInstance from "./services/web-socket-instance";
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { connected: false };
+  }
+
+  componentDidMount() {
+    const webSocketInstance = getWebSocketInstance("http://10.0.2.2:4444/")
+    const remoteMouseService = RemoteMouseService(webSocketInstance)
+    this.setState({remoteMouseService}, () => {
+      this.state.remoteMouseService.start();
+    })
+
+  }
+
+  handleSendMessage = message => {
+    this.state.remoteMouseService.sendMessage(message)
+  }
+
+  render() {
+    const { remoteMouseService } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text>Remote mouse client</Text>
+
+        {remoteMouseService && <Button
+          title = 'Connect'
+          onPress = { () => this.handleSendMessage("hello") } />}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
