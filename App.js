7ee7cd3b7d819da076c76d4a1c8c3fcce7669ca0
sooraj <soorajdube.is@gmail.com>
2021-03-26 02:30:25 +0530

    Sooraj|Added functionality to send coordinates of simple trackpad to server

--- App.js
+++ App.js
@@ -1,46 +1,59 @@
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, View, TouchableHighlight } from 'react-native';
 import RemoteMouseService from './services/remote-mouse-service'
 import getWebSocketInstance from "./services/web-socket-instance";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { connected: false };
+const App = () => {
+
+  const [remoteMouseService, setRemoteMouseService] = useState();
+
+  useEffect(() => {
+    const webSocketInstance = getWebSocketInstance("http://10.0.2.2:4444/?clientName=MobileClient")
+    setRemoteMouseService(RemoteMouseService(webSocketInstance));
+  }, [])
+
+  useEffect(() => {
+    if(remoteMouseService)
+      remoteMouseService.start();
+  },[remoteMouseService])
+
+  const handleTrackpadTouch = event => {
+    const location = `${event.nativeEvent.locationX} ${event.nativeEvent.locationY}`
+    remoteMouseService.sendMessage(location)
   }
 
-  componentDidMount() {
-    const webSocketInstance = getWebSocketInstance("http://10.0.2.2:4444/")
-    const remoteMouseService = RemoteMouseService(webSocketInstance)
-    this.setState({remoteMouseService}, () => {
-      this.state.remoteMouseService.start();
-    })
+  return (
+    <View style={styles.container}>
 
-  }
+      {remoteMouseService &&
+        <TouchableHighlight
+            activeOpacity={0.6}
+            underlayColor="#DDDDDD"
+            onPress={(event) => handleTrackpadTouch(event)}
+            style={styles.trackpad}>
+          <View >
 
-  handleSendMessage = message => {
-    this.state.remoteMouseService.sendMessage(message)
-  }
-
-  render() {
-    const { remoteMouseService } = this.state;
-    return (
-      <View style={styles.container}>
-        <Text>Remote mouse client</Text>
-
-        {remoteMouseService && <Button
-          title = 'Connect'
-          onPress = { () => this.handleSendMessage("hello") } />}
-      </View>
-    );
-  }
+          </View>
+        </TouchableHighlight>
+      }
+    </View>
+  );
 }
+export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#12151F',
+    color: '#05F4B7',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  trackpad: {
+    width: '90%',
+    height: '70%',
+    borderColor: '#05F4B7',
+    borderWidth: 3,
+    borderRadius: 20,
+  }
 });
