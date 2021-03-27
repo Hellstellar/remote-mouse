afe00c96ee1874a7f9f9c8ed5224fe51fcaa3118
sooraj <soorajdube.is@gmail.com>
2021-03-27 16:12:47 +0530

    Sooraj|Added functionality to send positions to move the mouse

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import React, {useEffect, useState} from 'react';
 import { StyleSheet, View} from 'react-native';
 import RemoteMouseService from './services/remote-mouse-service'
 import getWebSocketInstance from "./services/web-socket-instance";
+//import debounce from 'lodash.debounce';
 
 const App = () => {
 
   const [remoteMouseService, setRemoteMouseService] = useState();
+  let prevPositionX = 0;
+  let prevPositionY = 0;
+  let isTouched = false
 
   useEffect(() => {
-    const webSocketInstance = getWebSocketInstance("http://10.0.2.2:4444/?clientName=MobileClient")
+    const webSocketInstance = getWebSocketInstance("http://192.168.1.11:4444/?clientName=MobileClient")
     setRemoteMouseService(RemoteMouseService(webSocketInstance));
   }, [])
 
@@ -17,19 +21,45 @@ const App = () => {
       remoteMouseService.start();
   },[remoteMouseService])
 
-  const handleTrackpadTouch = event => {
-    const location = `${event.nativeEvent.locationX} ${event.nativeEvent.locationY}`
+  const getDeltaPositionXY = ({locationX, locationY}) => {
+    const currentPositionX = (locationX).toFixed()
+    const currentPositionY = (locationY).toFixed()
+    const deltaX = currentPositionX - prevPositionX
+    const deltaY = currentPositionY - prevPositionY
+    prevPositionX = currentPositionX
+    prevPositionY = currentPositionY
+    if(!isTouched) {
+      isTouched = true;
+      return [0, 0]
+    }
+    return [deltaX, deltaY];
+  };
+
+
+  const handleTrackpadTouch = nativeEvent => {
+    const [deltaX, deltaY] = getDeltaPositionXY(nativeEvent);
+    const location = `${deltaX} ${deltaY}`
     remoteMouseService.sendMessage(location)
   }
+
+  const handleTouchRelease = () => {
+    console.log('release')
+    isTouched = false
+    prevPositionX = 0;
+    prevPositionY = 0;
+  }
+
+  //const delayedHandleTouchMove = debounce(event => handleTrackpadTouch(event), 10);
+
 
   return (
     <View style={styles.container}>
 
       {remoteMouseService &&
-
           <View
               onStartShouldSetResponder={() => true}
-              onResponderMove={(evt) => handleTrackpadTouch(evt)}
+              onResponderMove={evt => handleTrackpadTouch(evt.nativeEvent)}
+              onResponderRelease={handleTouchRelease}
               style={styles.trackpad}>
           </View>
       }
